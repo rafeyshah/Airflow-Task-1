5b9f5ae69acf9a4976af0f09de3827bc1a8483b1
Your Name <csyedabdulrafeyali@gmail.com>
2021-07-27 20:59:50 +0500

    2nd

--- dags/tuto.py
+++ dags/tuto.py
@@ -23,5 +23,25 @@ default_args = {
 
 dag = DAG("tutorial", default_args=default_args, schedule_interval=timedelta(1))
 
-for x in range(6):
-  print(x)
+# t1, t2 and t3 are examples of tasks created by instantiating operators
+t1 = BashOperator(task_id="print_date", bash_command="date", dag=dag)
+
+t2 = BashOperator(task_id="sleep", bash_command="sleep 5", retries=3, dag=dag)
+
+templated_command = """
+    {% for i in range(5) %}
+        echo "{{ ds }}"
+        echo "{{ macros.ds_add(ds, 7)}}"
+        echo "{{ params.my_param }}"
+    {% endfor %}
+"""
+
+t3 = BashOperator(
+    task_id="templated",
+    bash_command=templated_command,
+    params={"my_param": "Parameter I passed in"},
+    dag=dag,
+)
+
+t2.set_upstream(t1)
+t3.set_upstream(t1)
